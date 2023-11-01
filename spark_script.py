884bbebbbceaa0694955d95977db09d2931e86f6
Thanan Sapthamrong <thanan.s@lmwn.com>
2023-11-01 11:45:22 +0700

    fix wrong path

--- spark_script.py
+++ spark_script.py
@@ -9,12 +9,12 @@ from pathlib import Path
 from pyspark.sql import SparkSession
 
 url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet'
-data_dir = '/home/data/nyc_taxi_data/yellow_tripdata'
-Path("data_dir").mkdir(parents=True, exist_ok=True)
+data_dir = '/home/data/nyc_taxi_data/yellow_tripdata/'
+Path(data_dir).mkdir(parents=True, exist_ok=True)
 
 print(f"downloading dataset from {url}")
 r = requests.get(url)  
-with open(data_dir, 'wb') as f:
+with open(f"{data_dir}/yellow_tripdata_2023-01.parquet", 'wb') as f:
     f.write(r.content)
 
 spark = SparkSession.builder.appName('test').getOrCreate()
